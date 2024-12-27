7385359e42e98ff9954ed6648b7e51b0a3280928
M.Baran Bilal <mahfuzbaranbilal@gmail.com>
2024-12-28 01:25:41 +0300

    Add project files.

---
+++ Project2WooxTravel_New/Areas/Admin/Controllers/ProfileController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project2WooxTravel_New.Context;
+using Project2WooxTravel_New.Entities;
+
+namespace Project2WooxTravel_New.Areas.Admin.Controllers
+{ 
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        TravelContext context=new TravelContext();
+
+        // GET: Admin/Profile
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var a = Session["x"];
+            var values=context.Admins.Where(x=>x.UserName==a).FirstOrDefault();
+            return View(values);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Project2WooxTravel_New.Entities.Admin admin)
+        {
+			var a = Session["x"];
+			var values = context.Admins.Where(x => x.UserName == a).FirstOrDefault();
+            values.Name = admin.Name;   
+            values.Surname = admin.Surname;
+            values.Email = admin.Email;
+            values.UserName = admin.UserName;
+            values.Password = admin.Password;
+            context.SaveChanges();
+			return RedirectToAction("Index","Profile","Admin");
+        }
+    }
+}
---
+++ Project2WooxTravel_New/Areas/Admin/Controllers/ReservationController.cs
@@ -0,0 +1,20 @@
+﻿using Project2WooxTravel_New.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project2WooxTravel_New.Areas.Admin.Controllers
+{
+    public class ReservationController : Controller
+    {
+		TravelContext context = new TravelContext();
+		// GET: Admin/Reservation
+		public ActionResult ReservationList()
+        {
+            var values=context.Reservations.ToList();
+            return View(values);
+        }
+    }
+}
